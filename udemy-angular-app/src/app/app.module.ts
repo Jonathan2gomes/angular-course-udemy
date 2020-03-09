daf913d9d560f9fddc84f94abbcfca28e7c6cf85
jonathan2gomes <jonathan2gomes.jg@gmail.com>
2020-03-08 22:35:39 -0300

    some changes and learning how to create a component

--- udemy-angular-app/src/app/app.module.ts
+++ udemy-angular-app/src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TesteComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
-
-    BrowserModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
